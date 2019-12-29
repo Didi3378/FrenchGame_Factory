ddec79dcd66678da4351c736391a5d0ba78f37a3
Diewo Wandianga <diewo33@icloud.com>
2019-12-29 12:29:02 +0000

    Initial Commit

---
+++ myVideoGame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  myVideoGame
+//
+//  Created by Diewo Wandianga on 29/12/2019.
+//  Copyright © 2019 Diewo Wandianga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
